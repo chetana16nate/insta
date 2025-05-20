53604cb8a58499a7eff92f8183088f09b591511b
unknown <thasalechetana@gmail.com>
2025-05-20 04:15:12 +0000

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import './App.css'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import 'bootstrap/dist/css/bootstrap.min.css';
- import Contacts from './Components/Contacts';
+import logo from './logo.svg';
+import './App.css';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+//  import Contacts from './Components/Contacts';
 import Login from './Components/Login';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
