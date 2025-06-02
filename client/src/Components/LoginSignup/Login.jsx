e16ecf62e8bcb64a4beba0f6842b8b511e0d5942
unknown <thasalechetana@gmail.com>
2025-06-02 11:37:30 +0000

    button added

--- client/src/Components/LoginSignup/Login.jsx
+++ client/src/Components/LoginSignup/Login.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
-import './Login.css'
+import 'instagramImage' from './assets/instagram-logo.svg.png';
+import 'googlePlayImage' from './assets/googlePlay.png';
+import 'appStoreImage' from './assets/appStore.png';
+import 'facebookImage' from './assets/fb-icon.png';
+import '../App'
 
 const Login=()=> {
   return (
-    <div className='container'>
-      <div className='Header'>
-        <h1>hello</h1>
+    <div className='login-container'>
+      <div className='box-1'></div>
+      <div classname='box-1-logo'>
+        <img src={'instagramlogoImage'} alt='#' classname="instagra-logo"/>
       </div>
+      <div classname="input-box">
+        <input type='text' placeholder='phone number, username or email address'/>
+      </div>
+      <div classname="input-box">
+       <input type="password" placeholder="Password"/>
+      </div>
+      <div className='input-box '>
+        <input type="password" placeholder="Password"/>
+      </div>
+
+       <div className='login-button-box'>
+        <button className="login-button">Login</button>
+      </div>
+      <div className='box-2'></div>
+      <div className='get-app-box'></div>
+      <div className='app-store-google-play-box'></div>
       
     </div>
   )
-}
+};
 
-export default Login
+export default Login;
